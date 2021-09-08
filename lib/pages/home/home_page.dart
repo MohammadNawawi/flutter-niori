d23097f1c0fbf6adf482e6af380a47c978d1b982
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2021-09-08 17:10:42 +0700

    Create Home Page Header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,11 +5,52 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: textBlackStyle,
-      ),
+
+    Widget header(){
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Naw',
+                    style: textBlackStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@naw',
+                    style: textSubtitleStyle.copyWith(
+                      fontSize: 16,
+                    )
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage('assets/images/iclog/image_profile.png',),),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
