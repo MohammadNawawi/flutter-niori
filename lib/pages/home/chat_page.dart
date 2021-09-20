560bef59997185e69084b8c5121e53c4584be452
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2021-09-20 20:07:32 +0700

    Create Page Empty Chat

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,11 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:niori/theme.dart';
 
 class ChatPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Chat Page'),
-    );
+    Widget header() {
+      return AppBar(
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: textWhiteStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icon_headset.png',
+                width: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Oopss... belum ada pesan?!',
+                style: textWhiteStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Anda belum bertransaksi!',
+                style: textSubtitleStyle,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 24,
+                      ),
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      )),
+                  child: Text(
+                    'Kunjungi Toko!',
+                    style: textWhiteStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column();
   }
 }
